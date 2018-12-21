ef5fa1a76624209c7c0dd4abdb2f0ff944a24b05
jsdiuf <weichun713@foxmail.com>
2018-12-21 09:04:46 +0800

    update

--- feign/src/main/java/com/jsdiuf/feign/controller/FeignController.java
+++ feign/src/main/java/com/jsdiuf/feign/controller/FeignController.java
@@ -1,11 +1,12 @@
 package com.jsdiuf.feign.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class FeignController {
 
-    @Autowired
+    @Resource
     private TestFeignClient testFeignClient;
 
     @Autowired
@@ -22,9 +23,11 @@ public class FeignController {
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
+        //feign
         String string = this.testFeignClient.add();
         System.out.println(string);
-        //System.out.println(this.restTemplate.getForObject("http://service-a/test", String.class));
+        //ribbon
+        System.out.println(this.restTemplate.getForObject("http://service-a/test", String.class));
         return string;
     }
 }
